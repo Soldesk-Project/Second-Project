54a2bf1ded28f1978c9ca5fe980569550fdfa1fe
l2cr <coco7537@naver.com>
2025-06-20 17:24:14 +0900

    src 파일 추가

---
+++ second-project-react/src/components/Test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div>
+      <div>
+        <p>test</p>
+      </div>
+    </div>
+  );
+};
+
+export default Test;<div>
+<p>test</p></div>
