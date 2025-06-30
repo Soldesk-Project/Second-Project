bef0d2612adf997d21cf1511824ad2387b0c37ec
Jeong <arthurking9407@gmail.com>
2025-06-30 17:38:10 +0900

    채팅창 UI 1차 작업

---
+++ second-project-react/src/layout/Chatbox.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/chatbox.css';
+
+const Chatbox = () => {
+  return (
+    <div className="chat-container">
+      <button>전체</button>
+      <button>귓속말</button>
+      <div className="chat-screen">
+
+      </div>
+      <div className="chat-input">
+        <input type="text" id="chatInput"/>
+        <button id="emojiBtn" className="emojiBtn">😊</button>
+        <button id="sendBtn" className="sendBtn">전달</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chatbox;
