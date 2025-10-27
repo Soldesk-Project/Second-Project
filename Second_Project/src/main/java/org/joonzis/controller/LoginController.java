9fa94dcd075d7ec2d394887e6ec58102a37ed07f
rudals <rudalsfive@gmail.com>
2025-10-27 16:00:10 +0900

    ec2 api test

--- Second_Project/src/main/java/org/joonzis/controller/LoginController.java
+++ Second_Project/src/main/java/org/joonzis/controller/LoginController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,7 +47,6 @@ import lombok.extern.log4j.Log4j;
 
 @Log4j
 @RestController
-@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class LoginController {
 	private static final ConcurrentHashMap<String, HttpSession> loginSessions = new ConcurrentHashMap<>();
